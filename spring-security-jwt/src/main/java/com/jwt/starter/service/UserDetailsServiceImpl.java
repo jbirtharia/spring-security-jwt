f591492c53d25068e60e8c22042f39e0724d899a
jayendrabirtharia <jbirtharia@gmail.com>
2025-01-18 16:24:58 +0800

    Made few changes related to authentication

--- spring-security-jwt/src/main/java/com/jwt/starter/service/UserDetailsServiceImpl.java
+++ spring-security-jwt/src/main/java/com/jwt/starter/service/UserDetailsServiceImpl.java
@@ -1,14 +1,11 @@
 package com.jwt.starter.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.jwt.starter.entities.Users;
 import com.jwt.starter.repository.UserRepository;
 import com.jwt.starter.security.AuthenticatedUser;
 
@@ -20,15 +17,9 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-		Optional<Users> users = userRepository.findByUserName(username);
-
-		if(null != users && users.isPresent()) {
-			// If users found in DB, then create authenticate user object
-			return users.map(AuthenticatedUser::new).get();
-		} else {
-			throw new UsernameNotFoundException("User Not Found !!");
-		}
-
+		
+		return userRepository.findByUserName(username)
+				.map(AuthenticatedUser::new)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found !!"));
 	}
 }
